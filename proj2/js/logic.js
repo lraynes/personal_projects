10c072f7012ef3f7ca4e910b15a4d1499572db88
Laura <raynes.laura11@gmail.com>
2018-08-22 21:51:22 -0700

    updating composers, adding proj 2

---
+++ proj2/js/logic.js
@@ -0,0 +1,44 @@
+
+// Create the tile layer that will be the background of our map
+let satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.satellite",
+  accessToken: API_KEY
+});
+
+let lightmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.light",
+  accessToken: API_KEY
+});
+
+let outdoormap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.outdoors",
+  accessToken: API_KEY
+});
+
+// Create a baseMaps object to hold the lightmap layer
+let baseMaps = {
+  "Satellite": satellitemap,
+  "Light Map": lightmap,
+  "Outdoors": outdoormap
+};
+
+let myMap = L.map("map-id", {
+  center: [37.8, -96],
+  zoom: 4,
+  layers: [satellitemap]
+});
+
+L.control.layers(baseMaps).addTo(myMap);
+
+
+
+// var url = "https://leafletjs.com/examples/choropleth/us-states.js"
+
+
+L.geoJson(statesData).addTo(myMap);
